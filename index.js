848a5b32c1e3710f04868c7e9fab4e3da1388eb2
paulinegc <paulinegc@protonmail.com>
2021-11-29 20:36:30 +0100

    autoreview

--- index.js
+++ index.js
@@ -1,14 +1,12 @@
 import 'dotenv/config';
 import mongoose from 'mongoose';
 import app from './src/app';
-// import setupDatabase from './src/config/database.config';
 import logger from './src/modules/commons/services/logger.service';
 import config from './src/config/app.config';
 
 const { MONGO_URI } = config.database;
 
 async function createServer() {
-  // await setupDatabase();
   const PORT = 5500;
 
   mongoose.connect(MONGO_URI, {
