25c2dfd116aadf07751075e12c13430df72fb67a
folland87 <frederic@olland.me>
2022-01-18 10:16:42 +0100

    add localisations

--- src/modules/structures/structures.routes.js
+++ src/modules/structures/structures.routes.js
@@ -3,6 +3,7 @@ import structuresControllers from './controllers/structures.controllers';
 import statusesControllers from './controllers/status.controllers';
 import namesControllers from './controllers/names.controllers';
 import identifiersControllers from './controllers/identifiers.controllers';
+import localisationsControllers from './controllers/localisations.controllers';
 import { requireActiveUser } from '../commons/middlewares/rbac.middlewares';
 
 const router = new express.Router();
@@ -28,10 +29,17 @@ router.get('/structures/:structureId/names/:nameId', namesControllers.read);
 router.patch('/structures/:structureId/names/:nameId', namesControllers.update);
 
 // Identifiers
-router.get('/structures/:structureId/names', identifiersControllers.list);
-router.post('/structures/:structureId/names', identifiersControllers.create);
-router.delete('/structures/:structureId/names/:identifierId', identifiersControllers.delete);
-router.get('/structures/:structureId/names/:identifierId', identifiersControllers.read);
-router.patch('/structures/:structureId/names/:identifierId', identifiersControllers.update);
+router.get('/structures/:structureId/identifiers', identifiersControllers.list);
+router.post('/structures/:structureId/identifiers', identifiersControllers.create);
+router.delete('/structures/:structureId/identifiers/:identifierId', identifiersControllers.delete);
+router.get('/structures/:structureId/identifiers/:identifierId', identifiersControllers.read);
+router.patch('/structures/:structureId/identifiers/:identifierId', identifiersControllers.update);
+
+// Localisations
+router.get('/structures/:structureId/localisations', localisationsControllers.list);
+router.post('/structures/:structureId/localisations', localisationsControllers.create);
+router.delete('/structures/:structureId/localisations/:localisationId', localisationsControllers.delete);
+router.get('/structures/:structureId/localisations/:localisationId', localisationsControllers.read);
+router.patch('/structures/:structureId/localisations/:localisationId', localisationsControllers.update);
 
 export default router;
