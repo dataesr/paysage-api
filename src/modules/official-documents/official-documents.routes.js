2260ed9ab314c971b9c5a34f44eda00f17c7d8fe
folland87 <frederic@olland.me>
2022-01-19 10:51:28 +0100

    deal with currentName

--- src/modules/official-documents/official-documents.routes.js
+++ src/modules/official-documents/official-documents.routes.js
@@ -5,14 +5,11 @@ import { requireActiveUser } from '../commons/middlewares/rbac.middlewares';
 const router = new express.Router();
 
 // GLOBAL MIDDELWARES
-// router.use(requireActiveUser);
-const test = (req, res, next) => {
-  console.log('INFO');
-  next();
-};
+router.use(requireActiveUser);
+
 // OFFICIAL DOCUMENTS
 router.get('/official-documents', officialDocumentsControllers.list);
-router.post('/official-documents', test, officialDocumentsControllers.create);
+router.post('/official-documents', officialDocumentsControllers.create);
 router.get('/official-documents/:officialDocumentId', officialDocumentsControllers.read);
 router.patch('/official-documents/:officialDocumentId', officialDocumentsControllers.update);
 router.delete('/official-documents/:officialDocumentId', officialDocumentsControllers.delete);
