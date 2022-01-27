e61c1eae0202966b641af1a3565d4439a038329f
folland87 <frederic@olland.me>
2022-01-27 15:42:19 +0100

    prices

--- src/modules/commons/controllers/base.controllers.js
+++ src/modules/commons/controllers/base.controllers.js
@@ -1,68 +1,70 @@
 import { NotFoundError, ServerError } from '../errors';
-import events from '../repositories/events.repo';
-import catalogue from '../repositories/catalogue.repo';
+import events from '../services/events.service';
+import catalogue from '../services/catalogue.service';
 
 export default class BaseController {
-  #test = 3
+  #repository;
+
+  #storeContext;
+
+  #storeEvent;
+
   constructor(repository, { storeContext = true, storeEvent = true } = {}) {
-    this._repository = repository;
-    this._storeContext = storeContext;
-    this._storeEvent = storeEvent;
+    this.#repository = repository;
+    this.#storeContext = storeContext;
+    this.#storeEvent = storeEvent;
   }
 
-  create = async (req, res, next) => {
-    const { ctx } = req;
-    console.log(this.#test);
-    const id = await catalogue.getUniqueId('prices');
-    const data = this._storeContext ? { id, ...req.body, ...ctx } : { id, ...req.body };
-    const insertedId = await this._repository.create(data);
-    if (this._storeEvent) {
-      const nextState = await this._repository.get(insertedId, { useModel: 'writeModel' });
-      events.add({ userId: ctx.user, resource: `${req.path}/${id}`, action: 'create', nextState });
+  create = async (req, res) => {
+    const ctx = req.ctx || {};
+    const id = await catalogue.getUniqueId(this.#repository.collectionName);
+    const data = this.#storeContext ? { id, ...req.body, ...ctx } : { id, ...req.body };
+    const insertedId = await this.#repository.create(data);
+    if (this.#storeEvent) {
+      const nextState = await this.#repository.get(insertedId, { useModel: 'writeModel' });
+      events.create({ userId: ctx.user, resource: `${req.path}/${id}`, action: 'create', nextState });
     }
-    const resource = await this._repository.get(id);
+    const resource = await this.#repository.get(id);
     if (!resource) throw new ServerError();
     res.status(201).json(resource);
-  }
+  };
 
-  patch = async (req, res, next) => {
-    const { ctx } = req;
+  patch = async (req, res) => {
+    const ctx = req.ctx || {};
     const { id, ...rest } = req.params;
-    const data = this._storeContext ? { ...rest, ...req.body, ...ctx } : { ...rest, ...req.body };
-    const prevState = await this._repository.get(id, { useModel: 'writeModel' });
-    const { ok } = await this._repository.patch(id, data);
-    if (ok && this._storeEvent) {
-      const nextState = await this._repository.get(id, { useModel: 'writeModel' });
-      events.add({ userId: ctx.user, resource: `${req.path}/${id}`, action: 'patch', prevState, nextState });
+    const data = this.#storeContext ? { ...rest, ...req.body, ...ctx } : { ...rest, ...req.body };
+    const prevState = await this.#repository.get(id, { useModel: 'writeModel' });
+    const { ok } = await this.#repository.patch(id, data);
+    if (ok && this.#storeEvent) {
+      const nextState = await this.#repository.get(id, { useModel: 'writeModel' });
+      events.create({ userId: ctx.user, resource: `${req.path}/${id}`, action: 'patch', prevState, nextState });
     }
-    const resource = await this._repository.get(id);
+    const resource = await this.#repository.get(id);
     if (!resource) throw new ServerError();
-    res.status(201).json(resource);
-  }
+    res.status(200).json(resource);
+  };
 
-  delete = async (req, res, next) => {
-    const { ctx } = req;
+  delete = async (req, res) => {
+    const ctx = req.ctx || {};
     const { id } = req.params;
-    const prevState = await this._repository.get(id, { useModel: 'writeModel' });
-    const { ok } = await this._repository.remove(id);
-    if (ok && this._storeEvent) {
-      events.add({ userId: ctx.user, resource: `${req.path}/${id}`, action: 'remove', prevState });
+    const prevState = await this.#repository.get(id, { useModel: 'writeModel' });
+    const { ok } = await this.#repository.remove(id);
+    if (ok && this.#storeEvent) {
+      events.create({ userId: ctx.user, resource: `${req.path}/${id}`, action: 'delete', prevState });
     }
-    const resource = await this._repository.get(id);
-    if (!resource) throw new ServerError();
-    res.status(201).json(resource);
-  }
+    res.status(204).json();
+  };
 
-  read = async (req, res, next) => {
+  read = async (req, res) => {
     const { id } = req.params;
-    const resource = await this._repository.get(id);
+    const resource = await this.#repository.get(id);
     if (!resource) throw new NotFoundError();
     res.status(200).json(resource);
-  }
+  };
 
-  list = async (req, res, next) => {
+  list = async (req, res) => {
     const params = req.query;
-    const { data, totalCount } = await this._repository.find(params);
+    const { data, totalCount } = await this.#repository.find(params);
     res.status(200).json({ data, totalCount: totalCount || 0 });
-  }
+  };
 }
---
+++ src/modules/commons/services/events.service.js
@@ -0,0 +1,29 @@
+import MongoRepository from '../repositories/mongo.repository';
+
+const pipeline = [
+  {
+    $lookup: {
+      from: 'users',
+      localField: 'user',
+      foreignField: 'id',
+      as: 'user',
+    },
+  },
+  { $set: { user: { $arrayElemAt: ['$user', 0] } } },
+  {
+    $set: {
+      user:
+      {
+        id: { $ifNull: ['$user.id', null] },
+        username: { $ifNull: ['$user.username', null] },
+        avatar: { $ifNull: ['$user.avatar', null] },
+      },
+    },
+  },
+  { $set: { id: '$_id' } },
+  { $project: { userId: 0, _id: 0 } },
+];
+
+class EventRepository extends MongoRepository {}
+
+export default new EventRepository({ collection: 'events', models: { readModel: pipeline } });
