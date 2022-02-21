4ac98a0d19613266f2c1ca1339fc30e2e732e284
folland87 <frederic@olland.me>
2022-02-21 12:04:05 +0100

    change catalogue collections names

--- src/modules/commons/monster/index.js
+++ src/modules/commons/monster/index.js
@@ -1,8 +1,8 @@
 import db from '../../../services/mongo.service';
 import { Repository, Catalogue } from '../../../libs/monster';
 
-const objectCatalogue = new Catalogue({ db, collection: '_objects' }, 5);
-const internalCatalogue = new Catalogue({ db, collection: '_subobjects' }, 8);
+const objectCatalogue = new Catalogue({ db, collection: '_catalogue' }, 5);
+const internalCatalogue = new Catalogue({ db, collection: '_catalogue' }, 8);
 const fileCatalogue = new Catalogue({ db, collection: '_files' }, 12);
 const eventStore = new Repository({ db, collection: '_events' });
 
