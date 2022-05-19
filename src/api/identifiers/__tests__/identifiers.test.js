545d0b7803e330c7a13c9423c11d8ae57e553a98
Anne L'Hôte <anne.lhote@gmail.com>
2022-05-19 23:40:55 +0200

    feat(linkedObjects): List all identifiers for a given resourceId

--- src/api/identifiers/__tests__/identifiers.test.js
+++ src/api/identifiers/__tests__/identifiers.test.js
@@ -1,5 +1,5 @@
 let authorization;
-const rid = '42';
+const rid = 'AZERT';
 
 const payload = {
   type: 'Siret',
@@ -26,5 +26,38 @@ describe('API > identifiers > create', () => {
     expect(body.active).toBeTruthy();
     expect(body.resourceId).toBe(rid);
     expect(body.createdBy.username).toBe('user');
+  });
+});
+
+describe('API > identifiers > list', () => {
+  beforeAll(async () => {
+    await global.utils.db.collection('identifiers').deleteMany({});
+    await global.superapp
+      .post(`/identifiers/${rid}`)
+      .set('Authorization', authorization)
+      .send(payload)
+      .expect(201);
+    await global.superapp
+      .post(`/identifiers/${rid}`)
+      .set('Authorization', authorization)
+      .send({ ...payload, value: '12' })
+      .expect(201);
+    await global.superapp
+      .post(`/identifiers/${rid}`)
+      .set('Authorization', authorization)
+      .send({ ...payload, value: '42' })
+      .expect(201);
+  });
+  it('can list successfully', async () => {
+    const { body } = await global.superapp
+      .get(`/identifiers/${rid}`)
+      .set('Authorization', authorization)
+      .expect(200);
+    expect(body.data[0].resourceId).toBe(rid);
+    expect(body.data[0].value).toBe('12345678912345');
+    expect(body.data[1].resourceId).toBe(rid);
+    expect(body.data[1].value).toBe('12');
+    expect(body.data[2].resourceId).toBe(rid);
+    expect(body.data[2].value).toBe('42');
   });
 });
