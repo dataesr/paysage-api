f94db769c4ca4aa38782987ca0e136e8263bc5ea
Anne L'Hôte <anne.lhote@gmail.com>
2022-05-30 16:46:53 +0200

    feat(social-medias): Set persons social-medias as linked object

--- src/api/persons/socialmedias/socialmedias.repository.js
+++
@@ -1,9 +0,0 @@
-import mongo from '../../../services/mongo.service';
-import { NestedMongoRepository } from '../../../libs/monster';
-import config from '../persons.config';
-
-const { db } = mongo;
-const { collection, socialMediasField: field } = config;
-const personSocialMediasRepository = new NestedMongoRepository({ db, collection, field });
-
-export default personSocialMediasRepository;
