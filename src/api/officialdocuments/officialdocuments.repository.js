87790db1ab9069b6c61a779b4fc85f83d9976a93
folland87 <frederic@olland.me>
2022-06-01 16:09:49 +0200

    change officialDocuments to officialTexts

--- src/api/officialdocuments/officialdocuments.repository.js
+++
@@ -1,9 +0,0 @@
-import mongo from '../../services/mongo.service';
-import { BaseMongoRepository } from '../../libs/monster';
-import config from './officialdocuments.config';
-
-const { db } = mongo;
-const { collection } = config;
-const officialDocumentsRepository = new BaseMongoRepository({ db, collection });
-
-export default officialDocumentsRepository;
