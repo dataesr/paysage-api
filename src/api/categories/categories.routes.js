fc43664feaafea59f948b5fbbeb08bd9d15c4467
folland87 <frederic@olland.me>
2022-06-13 12:22:31 +0200

    feat(Nouvelles routes): Nouvelle route de gestion des medias sociaux des categories

--- src/api/categories/categories.routes.js
+++ src/api/categories/categories.routes.js
@@ -3,11 +3,13 @@ import express from 'express';
 import categories from './root/root.routes';
 import identifiers from './identifiers/identifiers.routes';
 import weblinks from './weblinks/weblinks.routes';
+import socialmedias from './social-medias/social-medias.routes';
 
 const router = new express.Router();
 
 router.use(categories);
 router.use(identifiers);
 router.use(weblinks);
+router.use(socialmedias);
 
 export default router;
