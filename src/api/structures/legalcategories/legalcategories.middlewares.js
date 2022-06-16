18cf4d0f40260ad80028b51daa4a1fb01e360f0c
Anne L'Hôte <anne.lhote@gmail.com>
2022-06-16 18:12:22 +0200

    feat(routes): Add new routes to add legal-category for a structure

---
+++ src/api/structures/legalcategories/legalcategories.middlewares.js
@@ -0,0 +1,25 @@
+import { BadRequestError } from '../../commons/http-errors';
+import legalCategoriesRepository from '../../legalcategories/legalcategories.repository';
+
+export function setStructureIdFromRequestPath(req, res, next) {
+  const { resourceId } = req.params;
+  req.context.structureId = resourceId;
+  return next();
+}
+
+export async function validatePayload(req, res, next) {
+  if (!Object.keys(req.body).length) throw new BadRequestError('Payload missing');
+  const { legalcategoryId } = req.body;
+  if (!legalcategoryId) return next();
+  const exists = await legalCategoriesRepository.get(legalcategoryId);
+  if (!exists) {
+    throw new BadRequestError(
+      'Referencing unknown resource',
+      [{
+        path: '.body.legalcategoryId',
+        message: `legal category '${legalcategoryId}' does not exist`,
+      }],
+    );
+  }
+  return next();
+}
---
+++ src/api/structures/legalcategories/legalcategories.routes.js
@@ -0,0 +1,40 @@
+import express from 'express';
+
+import { validatePayload, setStructureIdFromRequestPath } from './legalcategories.middlewares';
+import { readQuery } from './legalcategories.queries';
+import { createContext, patchContext, setGeneratedInternalIdInContext } from '../../commons/middlewares/context.middlewares';
+import controllers from '../../commons/middlewares/crud.middlewares';
+import { saveInStore } from '../../commons/middlewares/event.middlewares';
+import repository from '../../commons/repositories/relationships.repository';
+import config from '../structures.config';
+
+const { collection } = config;
+
+const router = new express.Router();
+
+router.route(`/${collection}/:resourceId/legal-categories`)
+  .get(controllers.list(repository, readQuery))
+  .post([
+    validatePayload,
+    createContext,
+    setStructureIdFromRequestPath,
+    setGeneratedInternalIdInContext('relationships'),
+    controllers.create(repository, readQuery),
+    saveInStore('relationships'),
+  ]);
+
+router.route(`/${collection}/:resourceId/legal-categories/:id`)
+  .delete([
+    patchContext,
+    controllers.remove(repository),
+    saveInStore('relationships'),
+  ])
+  .get(controllers.read(repository, readQuery))
+  .patch([
+    validatePayload,
+    patchContext,
+    controllers.patch(repository, readQuery),
+    saveInStore('relationships'),
+  ]);
+
+export default router;
