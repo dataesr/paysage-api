c52cbe5a542084ea5ea279534367fef8dbcd0305
folland87 <frederic@olland.me>
2022-08-25 15:24:08 +0200

    auth fixes (#77)

--- src/api/assets/assets.middlewares.js
+++ src/api/assets/assets.middlewares.js
@@ -1,7 +1,7 @@
 import storage from '../../services/storage.service';
 
 async function serveAsset(req, res) {
-  const file = await storage.download(req.url.slice(1));
+  const file = await storage.download(req.path.slice(1));
   file.pipe(res);
 }
 
