1ec13a84444bef52a58d045f0cb7d17ac052dbaa
folland87 <frederic@olland.me>
2022-09-15 11:20:32 +0200

    add queries for relations

---
+++ src/api/commons/queries/relation-types.light.js
@@ -0,0 +1,13 @@
+export default [
+  {
+    $project: {
+      _id: 0,
+      id: 1,
+      name: 1,
+      acronym: { $ifNull: ['$acronym', null] },
+      pluralName: { $ifNull: ['$pluralName', null] },
+      feminineName: { $ifNull: ['$feminineName', null] },
+      priority: 1,
+    },
+  },
+];
