3d071fbf727434ee846c4d7e48bef0319dc58086
Anne L'Hôte <anne.lhote@gmail.com>
2022-09-27 17:39:19 +0200

    fix(structures): Add creationOfficialText as nested object
    and closureOfficialText too

--- src/api/commons/queries/structures.query.js
+++ src/api/commons/queries/structures.query.js
@@ -1,22 +1,45 @@
 import metas from './metas.query';
 import currentLocalisationQuery from './current-localisation.query';
 import currentNameQuery from './current-name.query';
+import officialtextLightQuery from './officialtexts.light.query';
 
 export default [
   ...metas,
   ...currentNameQuery,
   ...currentLocalisationQuery,
   {
+    $lookup: {
+      from: 'officialtexts',
+      localField: 'creationOfficialTextId',
+      foreignField: 'id',
+      pipeline: officialtextLightQuery,
+      as: 'creationOfficialText',
+    },
+  },
+  { $set: { creationOfficialText: { $arrayElemAt: ['$creationOfficialText', 0] } } },
+  {
+    $lookup: {
+      from: 'officialtexts',
+      localField: 'closureOfficialTextId',
+      foreignField: 'id',
+      pipeline: officialtextLightQuery,
+      as: 'closureOfficialText',
+    },
+  },
+  { $set: { closureOfficialText: { $arrayElemAt: ['$closureOfficialText', 0] } } },
+  {
     $project: {
       _id: 0,
       id: 1,
       structureStatus: { $ifNull: ['$structureStatus', null] },
-      creationDate: { $ifNull: ['$creationDate', null] },
-      creationOfficialTextId: { $ifNull: ['$creationOfficialTextId', null] },
-      creationReason: { $ifNull: ['$creationReason', null] },
       closureDate: { $ifNull: ['$closureDate', null] },
       closureOfficialTextId: { $ifNull: ['$closureOfficialTextId', null] },
+      closureOfficialText: { $ifNull: ['$closureOfficialText', {}] },
       closureReason: { $ifNull: ['$closureReason', null] },
+      creationDate: { $ifNull: ['$creationDate', null] },
+      creationOfficialTextId: { $ifNull: ['$creationOfficialTextId', null] },
+      creationOfficialText: { $ifNull: ['$creationOfficialText', {}] },
+      creationReason: { $ifNull: ['$creationReason', null] },
       descriptionFr: { $ifNull: ['$descriptionFr', null] },
       descriptionEn: { $ifNull: ['$descriptionEn', null] },
       status: 1,
