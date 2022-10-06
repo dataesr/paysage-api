db143b765d66c24fa3dd3cc39437f85d943df60a
Anne L'Hôte <annelhote@users.noreply.github.com>
2022-10-06 17:42:12 +0200

    feat(keynumbers): Add real estate dataset (#99)

--- src/jobs/data.js
+++ src/jobs/data.js
@@ -7,33 +7,41 @@ const backupData = async (job, done) => {
     url: 'https://data.enseignementsup-recherche.gouv.fr/explore/dataset/fr-esr-operateurs-indicateurs-financiers/download/?format=json&timezone=Europe/Berlin&lang=en',
     field: 'resultat_net_comptable',
     fieldName: 'netAccountingResult',
+    paysageId: 'etablissement_id_paysage',
     sortField: 'exercice',
     sortFieldName: 'exercice',
   }, {
     url: 'https://data.enseignementsup-recherche.gouv.fr/explore/dataset/fr-esr-statistiques-sur-les-effectifs-d-etudiants-inscrits-par-etablissement-pay/download/?format=json&timezone=Europe/Berlin&lang=en',
     field: 'effectif',
     fieldName: 'population',
+    paysageId: 'etablissement_id_paysage',
     sortField: 'annee_universitaire',
     sortFieldName: 'academicYear',
+  }, {
+    url: 'https://data.enseignementsup-recherche.gouv.fr/explore/dataset/fr-esr-patrimoine-immobilier-des-operateurs-de-l-enseignement-superieur/download/?format=json&timezone=Europe/Berlin&lang=en',
+    paysageId: 'paysage_id',
   }];
   datasets.forEach(async (dataset) => {
     const response = await fetch(dataset.url, { headers: { Authorization: `Apikey ${process.env.ODS_API_KEY}` } });
     const data = await response.json();
-    const operationsKeyNumbers = data?.length && data.map((item) => ({
-      updateOne: {
-        filter: { id: { $eq: item.fields.etablissement_id_paysage } },
-        update: { $set: { ...item.fields, id: item.fields.etablissement_id_paysage, updatedAt: new Date() } },
-        upsert: true,
-      },
-    }));
+    const operationsKeyNumbers = data?.length && data.map((item) => {
+      const ids = item.fields[dataset.paysageId].split(',');
+      return ids.map((id) => ({
+        updateOne: {
+          filter: { id: { $eq: id } },
+          update: { $set: { ...item.fields, id, updatedAt: new Date() } },
+          upsert: true,
+        },
+      }));
+    });
     let operationsStructures = false;
     if (dataset?.field) {
       const uniqueStructures = [];
       operationsStructures = data?.length && data
         .sort((a, b) => (parseInt((b.fields[dataset.sortField]).slice(0, 4), 10) - parseInt((a.fields[dataset.sortField]).slice(0, 4), 10)))
         .filter((item) => {
-          if (!uniqueStructures.includes(item.fields.etablissement_id_paysage)) {
-            uniqueStructures.push(item.fields.etablissement_id_paysage);
+          if (!uniqueStructures.includes(item.fields[dataset.paysageId])) {
+            uniqueStructures.push(item.fields[dataset.paysageId]);
             return item;
           }
           return false;
@@ -45,7 +53,7 @@ const backupData = async (job, done) => {
           if (Object.keys(set).length > 0) {
             return {
               updateOne: {
-                filter: { id: { $eq: item.fields.etablissement_id_paysage } },
+                filter: { id: { $eq: item.fields[dataset.paysageId] } },
                 update: { $set: set },
               },
             };
