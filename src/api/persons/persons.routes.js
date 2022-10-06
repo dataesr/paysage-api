4a5ffead47d0eb730ab502645d0154802ae9ff23
folland87 <frederic@olland.me>
2022-10-06 08:34:08 +0200

    remove unnecessary routes

--- src/api/persons/persons.routes.js
+++ src/api/persons/persons.routes.js
@@ -1,6 +1,5 @@
 import express from 'express';
 
-import categories from './categories/categories.routes';
 import identifiers from './identifiers/identifiers.routes';
 import root from './root/root.routes';
 import socialmedias from './social-medias/social-medias.routes';
@@ -8,7 +7,6 @@ import weblinks from './weblinks/weblinks.routes';
 
 const router = new express.Router();
 
-router.use(categories);
 router.use(identifiers);
 router.use(root);
 router.use(socialmedias);
--- src/api/structures/structures.routes.js
+++ src/api/structures/structures.routes.js
@@ -1,33 +1,25 @@
 import express from 'express';
 
-import categories from './categories/categories.routes';
 import emails from './emails/emails.routes';
 import identifiers from './identifiers/identifiers.routes';
-import legalcategories from './legalcategories/legalcategories.routes';
 import localisations from './localisations/localisations.routes';
 import logos from './logos/logos.routes';
 import names from './names/names.routes';
-import relations from './relations/relations.routes';
 import relationsgroups from './relations-groups/relations-groups.routes';
 import socialmedias from './social-medias/social-medias.routes';
 import structures from './root/root.routes';
-import supervisingministers from './supervising-ministers/supervising-ministers.routes';
 import weblinks from './weblinks/weblinks.routes';
 
 const router = new express.Router();
 
-router.use(categories);
 router.use(emails);
 router.use(identifiers);
-router.use(legalcategories);
 router.use(localisations);
 router.use(logos);
 router.use(names);
-router.use(relations);
 router.use(relationsgroups);
 router.use(socialmedias);
 router.use(structures);
-router.use(supervisingministers);
 router.use(weblinks);
 
 export default router;
