0ee2b5a02722e637367f6b428e6a27574b06ce5c
folland87 <frederic@olland.me>
2022-10-06 08:33:42 +0200

    add a relations generic route

---
+++ src/api/relations/relations.routes.js
@@ -0,0 +1,36 @@
+import express from 'express';
+
+import { createContext, patchContext, setGeneratedInternalIdInContext } from '../commons/middlewares/context.middlewares';
+import { saveInStore } from '../commons/middlewares/event.middlewares';
+import controllers from '../commons/middlewares/crud.middlewares';
+import { validatePayload } from './relations.middlewares';
+import readQuery from '../commons/queries/relations.query';
+import { relationshipsRepository as repository } from '../commons/repositories';
+import { relations as resource } from '../resources';
+
+const router = new express.Router();
+
+router.route(`/${resource}`)
+  .get(controllers.list(repository, readQuery))
+  .post([
+    validatePayload,
+    createContext,
+    setGeneratedInternalIdInContext(resource),
+    controllers.create(repository, readQuery),
+    saveInStore(resource),
+  ]);
+
+router.route(`/${resource}/:id`)
+  .get(controllers.read(repository, readQuery))
+  .patch([
+    patchContext,
+    controllers.patch(repository, readQuery),
+    saveInStore(resource),
+  ])
+  .delete([
+    patchContext,
+    controllers.remove(repository),
+    saveInStore(resource),
+  ]);
+
+export default router;
