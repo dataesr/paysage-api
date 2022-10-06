9c2e0ff0f0be20e6acbed4a7afd7592c703c9d1d
Anne L'Hôte <anne.lhote@gmail.com>
2022-10-06 13:05:48 +0200

    fix(keynumbers): Make our key numbers great again

--- src/jobs/data.js
+++ src/jobs/data.js
@@ -11,7 +11,7 @@ const backupData = async (job, done) => {
     sortFieldName: 'exercice',
   }, {
     url: 'https://data.enseignementsup-recherche.gouv.fr/explore/dataset/fr-esr-statistiques-sur-les-effectifs-d-etudiants-inscrits-par-etablissement-pay/download/?format=json&timezone=Europe/Berlin&lang=en',
-    field: 'population',
+    field: 'effectif',
     fieldName: 'population',
     sortField: 'annee_universitaire',
     sortFieldName: 'academicYear',
@@ -21,30 +21,43 @@ const backupData = async (job, done) => {
     const data = await response.json();
     const operationsKeyNumbers = data?.length && data.map((item) => ({
       updateOne: {
-        filter: { etablissement_id_paysage: { $eq: item.fields.etablissement_id_paysage } },
+        filter: { id: { $eq: item.fields.etablissement_id_paysage } },
         update: { $set: { ...item.fields, id: item.fields.etablissement_id_paysage, updatedAt: new Date() } },
         upsert: true,
       },
     }));
-    const uniqueStructures = [];
-    const operationsStructures = data?.length && data
-      .sort((a, b) => b[dataset.sortField] - a[dataset.sortField])
-      .filter((item) => {
-        if (!uniqueStructures.includes(item.fields.etablissement_id_paysage)) {
-          uniqueStructures.push(item.fields.etablissement_id_paysage);
-          return item;
-        }
-        return false;
-      })
-      .map((item) => ({
-        updateOne: {
-          filter: { etablissement_id_paysage: { $eq: item.fields.etablissement_id_paysage } },
-          update: { $set: { [dataset.field]: item.fields[dataset.field], [dataset.sortField]: item.fields[dataset.sortField] } },
-        },
-      }));
+    let operationsStructures = false;
+    if (dataset?.field) {
+      const uniqueStructures = [];
+      operationsStructures = data?.length && data
+        .sort((a, b) => (parseInt((b.fields[dataset.sortField]).slice(0, 4), 10) - parseInt((a.fields[dataset.sortField]).slice(0, 4), 10)))
+        .filter((item) => {
+          if (!uniqueStructures.includes(item.fields.etablissement_id_paysage)) {
+            uniqueStructures.push(item.fields.etablissement_id_paysage);
+            return item;
+          }
+          return false;
+        })
+        .map((item) => {
+          const set = {};
+          if (dataset && dataset?.field && item && item?.fields?.[dataset.field]) set[dataset.fieldName] = item.fields[dataset.field];
+          if (dataset && dataset?.sortField && item && item?.fields?.[dataset.sortField]) set[dataset.sortFieldName] = item.fields[dataset.sortField];
+          if (Object.keys(set).length > 0) {
+            return {
+              updateOne: {
+                filter: { id: { $eq: item.fields.etablissement_id_paysage } },
+                update: { $set: set },
+              },
+            };
+          }
+          return {};
+        });
+    }
     try {
       await db.collection('keynumbers').bulkWrite(operationsKeyNumbers, { ordered: false });
-      await db.collection('structures').bulkWrite(operationsStructures, { ordered: false });
+      if (operationsStructures) {
+        await db.collection('structures').bulkWrite(operationsStructures, { ordered: false });
+      }
       logger.info('Data setup successful');
       done();
     } catch (e) {
