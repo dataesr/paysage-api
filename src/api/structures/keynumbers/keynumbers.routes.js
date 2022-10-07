79f38f4ad71cdd16db4197ceeed858c725a7fa27
Anne L'Hôte <anne.lhote@gmail.com>
2022-10-07 11:44:55 +0200

    fix(keynumbers): Translate API doc and add dataset into the endpoint

--- src/api/structures/keynumbers/keynumbers.routes.js
+++ src/api/structures/keynumbers/keynumbers.routes.js
@@ -1,33 +1,13 @@
 import express from 'express';
 
-import { createContext, patchContext, setGeneratedInternalIdInContext } from '../../commons/middlewares/context.middlewares';
-import { saveInStore } from '../../commons/middlewares/event.middlewares';
 import controllers from '../../commons/middlewares/crud.middlewares';
 import { keynumbersRepository as repository } from '../../commons/repositories';
 import { structures as resource, keynumbers as subresource } from '../../resources';
+import { setFilters } from './keynumbers.middleware';
 
 const router = new express.Router();
 
-router.route(`/${resource}/:resourceId/${subresource}`)
-  .get(controllers.list(repository))
-  .post([
-    createContext,
-    setGeneratedInternalIdInContext(subresource),
-    controllers.create(repository),
-    saveInStore(subresource),
-  ]);
-
-router.route(`/${resource}/:resourceId/${subresource}/:id`)
-  .get(controllers.read(repository))
-  .patch([
-    patchContext,
-    controllers.patch(repository),
-    saveInStore(subresource),
-  ])
-  .delete([
-    patchContext,
-    controllers.remove(repository),
-    saveInStore(subresource),
-  ]);
+router.route(`/${resource}/:resourceId/${subresource}/:dataset`)
+  .get(setFilters, controllers.list(repository));
 
 export default router;
