79a06a3fd79b5b43a456e427dbd7c22019cc8107
Anne L'Hôte <anne.lhote@gmail.com>
2022-10-27 16:24:20 +0200

    fix(es): Index only instead of delete

--- src/api/commons/middlewares/event.middlewares.js
+++ src/api/commons/middlewares/event.middlewares.js
@@ -4,28 +4,6 @@ import esClient from '../../../services/elastic.service';
 
 const { index } = config.elastic;
 
-const getDescriptionByType = (type, resource) => {
-  const texts = [];
-  texts.push(resource?.name);
-  if (type === 'structures') {
-    // Structures : Nom usuel + sigle ou nom court - Catégorie principale - Localisation - date de création
-    texts.push(resource?.acronym);
-    texts.push(resource?.locality?.join(' '));
-    texts.push(resource?.creationDate);
-  } else if (type === 'persons') {
-    // Personnes : Prénom, nom - dernier mandat renseigné ou activité récupérée de wikidata - structure associée au mandat
-  } else if (type === 'categories' || type === 'terms') {
-    // Catégories & termes : Nom usuel
-  } else if (type === 'official-texts') {
-    // Textes officiels : Libellé du texte officiel - structures associées
-  } else if (type === 'projects') {
-    // Projet : Nom usuel + sigle ou nom court du projet - Catégorie principale - Localisation - Date de début
-    texts.push(resource?.acronym);
-    texts.push(resource?.startDate);
-  }
-  return texts.filter((item) => ![null, ''].includes(item)).join(' - ');
-};
-
 export function saveInElastic(repository, useQuery, resourceName) {
   return async (req, res, next) => {
     const { body, params } = req || {};
@@ -45,17 +23,13 @@ export function saveInElastic(repository, useQuery, resourceName) {
       acronym: resource?.acronym,
       isDeleted: resource?.isDeleted || false,
       name: resource?.name,
-      description: getDescriptionByType(resourceName, resource),
     };
     if (resourceName === 'structures') {
       action.creationDate = resource?.creationDate;
       action.locality = resource?.locality?.[0];
     }
     const actions = [];
-    if (_id) {
-      actions.push({ delete: { _index: index, _id } });
-    }
-    actions.push({ index: { _index: index } });
+    actions.push(_id ? { index: { _index: index, _id } } : { index: { _index: index } });
     actions.push(action);
     await esClient.bulk({ refresh: true, body: actions });
     return next();
