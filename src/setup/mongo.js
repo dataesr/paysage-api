83404717850e70b8490df31d85aadca4da07cf54
folland87 <frederic@olland.me>
2022-10-27 15:28:52 +0200

    update mongo index to imrove performance

--- src/setup/mongo.js
+++ src/setup/mongo.js
@@ -8,11 +8,19 @@ async function setupMongo() {
   await db.collection('documents').createIndex({ id: 1 }, { unique: true });
   await db.collection('documenttypes').createIndex({ id: 1 }, { unique: true });
   await db.collection('emails').createIndex({ id: 1 }, { unique: true });
+  await db.collection('emails').createIndex({ resourceId: 1 });
   await db.collection('emailtypes').createIndex({ id: 1 }, { unique: true });
   await db.collection('identifiers').createIndex({ id: 1 }, { unique: true });
+  await db.collection('identifiers').createIndex({ resourceId: 1 });
+  await db.collection('identifiers').createIndex({ resourceId: 1, type: 1 });
   await db.collection('keynumbers').createIndex({ dataset: 1, resourceId: 1 });
   await db.collection('legalcategories').createIndex({ id: 1 }, { unique: true });
   await db.collection('officialtexts').createIndex({ id: 1 }, { unique: true });
+  await db.collection('officialtexts').createIndex({ id: 1, relatesTo: 1 });
+  await db.collection('followups').createIndex({ id: 1 }, { unique: true });
+  await db.collection('followups').createIndex({ id: 1, relatesTo: 1 });
+  await db.collection('documents').createIndex({ id: 1 }, { unique: true });
+  await db.collection('documents').createIndex({ id: 1, relatesTo: 1 });
   await db.collection('persons').createIndex({ id: 1 }, { unique: true });
   await db.collection('prices').createIndex({ id: 1 }, { unique: true });
   await db.collection('projects').createIndex({ id: 1 }, { unique: true });
@@ -21,7 +29,12 @@ async function setupMongo() {
   await db.collection('relationships').createIndex({ resourceId: 1, relationTag: 1 });
   await db.collection('relationships').createIndex({ relatedObjectId: 1 });
   await db.collection('relationships').createIndex({ relatedObjectId: 1, relationTag: 1 });
+  await db.collection('relationships').createIndex({ otherAssociatedObjectIds: 1, relationTag: 1 });
+  await db.collection('relationships').createIndex({ relationsGroupId: 1 });
+  await db.collection('relationtypes').createIndex({ id: 1 }, { unique: true });
+  await db.collection('relationtypes').createIndex({ for: 1 });
   await db.collection('socialmedias').createIndex({ id: 1 }, { unique: true });
+  await db.collection('socialmedias').createIndex({ resourceId: 1 });
   await db.collection('structures').createIndex({ id: 1 }, { unique: true });
   await db.collection('supervisingministers').createIndex({ id: 1 }, { unique: true });
   await db.collection('terms').createIndex({ id: 1 }, { unique: true });
@@ -31,7 +44,9 @@ async function setupMongo() {
   await db.collection('users').createIndex({ id: 1 }, { unique: true });
   await db.collection('groups').createIndex({ id: 1 }, { unique: true });
   await db.collection('groupmembers').createIndex({ userId: 1, groupId: 1 }, { unique: true });
+  await db.collection('groupmembers').createIndex({ userId: 1 });
   await db.collection('weblinks').createIndex({ id: 1 }, { unique: true });
+  await db.collection('weblinks').createIndex({ resourceId: 1 });
   logger.info('Mongodb setup successful');
   if (client) {
     await client.close();
