5fb452c47112adc499f7790b08fce8108a14a501
folland87 <frederic@olland.me>
2022-10-27 08:50:03 +0200

    feat(projects): Add identifiers to projects

---
+++ src/api/projects/identifiers/identifiers.routes.js
@@ -0,0 +1,34 @@
+import express from 'express';
+
+import { createContext, patchContext, setGeneratedInternalIdInContext } from '../../commons/middlewares/context.middlewares';
+import { saveInStore } from '../../commons/middlewares/event.middlewares';
+import controllers from '../../commons/middlewares/crud.middlewares';
+import { identifiersRepository as repository } from '../../commons/repositories';
+import readQuery from '../../commons/queries/identifiers.query';
+import { projects as resource, identifiers as subresource } from '../../resources';
+
+const router = new express.Router();
+
+router.route(`/${resource}/:resourceId/${subresource}`)
+  .get(controllers.list(repository, readQuery))
+  .post([
+    createContext,
+    setGeneratedInternalIdInContext(subresource),
+    controllers.create(repository, readQuery),
+    saveInStore(subresource),
+  ]);
+
+router.route(`/${resource}/:resourceId/${subresource}/:id`)
+  .get(controllers.read(repository, readQuery))
+  .patch([
+    patchContext,
+    controllers.patch(repository, readQuery),
+    saveInStore(subresource),
+  ])
+  .delete([
+    patchContext,
+    controllers.remove(repository),
+    saveInStore(subresource),
+  ]);
+
+export default router;
