f5880f80fb1988d12823ba8e931e8b0998ae8d3e
folland87 <frederic@olland.me>
2022-11-02 09:48:59 +0100

    add project social medias

--- src/api/projects/projects.routes.js
+++ src/api/projects/projects.routes.js
@@ -4,6 +4,7 @@ import root from './root/root.routes';
 import weblinks from './weblinks/weblinks.routes';
 import localisations from './localisations/localisations.routes';
 import identifiers from './identifiers/identifiers.routes';
+import socialmedias from './social-medias/social-medias.routes';
 
 const router = new express.Router();
 
@@ -11,5 +12,6 @@ router.use(root);
 router.use(weblinks);
 router.use(localisations);
 router.use(identifiers);
+router.use(socialmedias);
 
 export default router;
