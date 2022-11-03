8f72303ba716cc29c8f95b5c132ab3fff190ec08
folland87 <frederic@olland.me>
2022-11-03 16:52:41 +0100

    ministers to related elements

--- src/api/commons/queries/supervising-ministers.light.query.js
+++ src/api/commons/queries/supervising-ministers.light.query.js
@@ -1,10 +1,9 @@
-export default [
-  {
-    $project: {
-      _id: 0,
-      id: 1,
-      usualName: 1,
-      otherNames: { $ifNull: ['$otherNames', []] },
-    },
+export default [{
+  $project: {
+    _id: 0,
+    id: 1,
+    displayName: '$usualName',
+    collection: 'supervising-ministers',
+    href: { $concat: ['/supervising-ministers/', '$id'] },
   },
-];
+}];
