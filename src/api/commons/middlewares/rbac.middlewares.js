21a761575c808ef38ef9ee59b238b27297df99be
folland87 <frederic@olland.me>
2022-11-07 14:30:34 +0100

    add maleName for relation-types

--- src/api/commons/middlewares/rbac.middlewares.js
+++ src/api/commons/middlewares/rbac.middlewares.js
@@ -4,6 +4,7 @@ export function requireAuth(req, res, next) {
   if (['testing'].includes(process.env.NODE_ENV)) return next();
   if (['/signup', '/signin', '/token', '/recovery/password'].includes(req.path)) return next();
   if (req.path.startsWith('/assets/avatars')) return next();
+  if (req.path.startsWith('/assets/logos')) return next();
   if (!req?.currentUser?.id) {
     throw new UnauthorizedError('You must be connected');
   }
