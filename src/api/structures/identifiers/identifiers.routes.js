d02a86dd40591ac210d1c1aabafb7bd644225b23
Anne L'Hôte <anne.lhote@gmail.com>
2022-11-10 09:41:44 +0100

    feat(es): Trigger structure reindex on identifiers change

--- src/api/structures/identifiers/identifiers.routes.js
+++ src/api/structures/identifiers/identifiers.routes.js
@@ -1,10 +1,11 @@
 import express from 'express';
 
 import { createContext, patchContext, setGeneratedInternalIdInContext } from '../../commons/middlewares/context.middlewares';
-import { saveInStore } from '../../commons/middlewares/event.middlewares';
+import { saveInElastic, saveInStore } from '../../commons/middlewares/event.middlewares';
 import controllers from '../../commons/middlewares/crud.middlewares';
-import { identifiersRepository as repository } from '../../commons/repositories';
+import { identifiersRepository as repository, structuresRepository } from '../../commons/repositories';
 import readQuery from '../../commons/queries/identifiers.query';
+import elasticQuery from '../../commons/queries/structures.elastic';
 import { structures as resource, identifiers as subresource } from '../../resources';
 
 const router = new express.Router();
@@ -16,6 +17,7 @@ router.route(`/${resource}/:resourceId/${subresource}`)
     setGeneratedInternalIdInContext(subresource),
     controllers.create(repository, readQuery),
     saveInStore(subresource),
+    saveInElastic(structuresRepository, elasticQuery, resource),
   ]);
 
 router.route(`/${resource}/:resourceId/${subresource}/:id`)
@@ -24,11 +26,13 @@ router.route(`/${resource}/:resourceId/${subresource}/:id`)
     patchContext,
     controllers.patch(repository, readQuery),
     saveInStore(subresource),
+    saveInElastic(structuresRepository, elasticQuery, resource),
   ])
   .delete([
     patchContext,
     controllers.remove(repository),
     saveInStore(subresource),
+    saveInElastic(structuresRepository, elasticQuery, resource),
   ]);
 
 export default router;
