d857d4b51a1d1edf5e78ab4227197f5b636487e9
Anne L'Hôte <anne.lhote@gmail.com>
2022-11-29 17:12:18 +0100

    feat(projects): Index main category

--- src/api/commons/queries/projects.elastic.js
+++ src/api/commons/queries/projects.elastic.js
@@ -18,12 +18,33 @@ export default [
     },
   },
   {
+    $lookup: {
+      from: 'relationships',
+      localField: 'id',
+      foreignField: 'resourceId',
+      as: 'relationships',
+      pipeline: [{ $match: { relationTag: 'projet-categorie' } }],
+    },
+  },
+  { $set: { relationships: '$relationships.relatedObjectId' } },
+  {
+    $lookup: {
+      from: 'categories',
+      localField: 'relationships',
+      foreignField: 'id',
+      as: 'categories',
+      pipeline: [{ $sort: { priority: 1 } }],
+    },
+  },
+  { $set: { category: { $arrayElemAt: ['$categories', 0] } } },
+  {
     $project: {
       _id: 0,
       id: 1,
       acronym: { $ifNull: ['$acronymFr', null] },
       acronymEn: { $ifNull: ['$acronymEn', null] },
       acronymFr: { $ifNull: ['$acronymFr', null] },
+      category: { $ifNull: ['$category.usualNameFr', null] },
       fullNameEn: { $ifNull: ['$fullNameEn', null] },
       fullNameFr: { $ifNull: ['$fullNameFr', null] },
       identifiers: { $ifNull: ['$identifiers.value', null] },
