ba47ef82a5d6c2a0d4df8449d907807592d2b654
folland87 <frederic@olland.me>
2022-12-01 10:31:34 +0100

    requireRoles

--- src/api/contacts/contacts.routes.js
+++ src/api/contacts/contacts.routes.js
@@ -1,4 +1,6 @@
 import express from 'express';
+import rateLimit from 'express-rate-limit';
+
 import { createContext, setGeneratedInternalIdInContext } from '../commons/middlewares/context.middlewares';
 import { saveInStore } from '../commons/middlewares/event.middlewares';
 import { requireRoles } from '../commons/middlewares/rbac.middlewares';
@@ -10,9 +12,17 @@ import controllers from '../commons/middlewares/crud.middlewares';
 
 const router = new express.Router();
 
+const maxRequestsPerHour = (max) => rateLimit({
+  windowMs: 60 * 60 * 1000,
+  max,
+  message: 'Trop de requêtes, essayez à nouveau dans une heure.',
+});
+
 router.route(`/${resource}`)
   .get(requireRoles(['admin']), controllers.list(repository, readQuery))
   .post([
+    maxRequestsPerHour(5),
+    requireRoles(['user', 'admin']),
     sendContactMessageByEmail,
     createContext,
     setGeneratedInternalIdInContext(resource),
