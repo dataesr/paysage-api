684a07567f64c5cc8bf9884421db780ffc1b4ddd
folland87 <frederic@olland.me>
2022-12-12 18:18:11 +0100

    feat(auth): add apiKeys feature

---
+++ src/api/apikeys/apikeys.middlewares.js
@@ -0,0 +1,15 @@
+import crypto from 'crypto';
+
+const CHARS = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'.repeat(5);
+
+export function setApiKeyInContext(req, res, next) {
+  const rand = crypto.randomBytes(20);
+  let str = '';
+
+  for (let i = 0; i < rand.length; i += 1) {
+    const decimal = rand[i];
+    str += CHARS[decimal];
+  }
+  req.context.apiKey = `xkeypsg-${str}`;
+  return next();
+}
--- src/api/commons/middlewares/authenticate.middlewares.js
+++ src/api/commons/middlewares/authenticate.middlewares.js
@@ -1,18 +1,27 @@
 import jwt from 'jsonwebtoken';
-
+import { apiKeysRepository } from '../repositories';
 import config from '../../../config';
 
 const { jwtSecret } = config;
 
 export async function authenticate(req, res, next) {
-  const { authorization } = req.headers;
+  const { authorization, 'x-api-key': xApiKey } = req.headers;
   req.currentUser = {};
-  try {
-    const token = authorization.replace('Bearer ', '');
-    const decodedToken = jwt.verify(token, jwtSecret);
-    req.currentUser = decodedToken.user;
-  } catch (e) {
+  if (xApiKey) {
+    console.log('HEREEEEEEE');
+    const apiKey = await apiKeysRepository.find({ filters: { apiKey: xApiKey } });
+    if (apiKey.userId) req.currentUser = { id: apiKey.userId, role: apiKey.role };
     return next();
+  }
+  if (authorization) {
+    console.log('TTTTTTTHEREEEEEEE');
+    try {
+      const token = authorization.replace('Bearer ', '');
+      const decodedToken = jwt.verify(token, jwtSecret);
+      req.currentUser = decodedToken.user;
+    } catch (e) {
+      return next();
+    }
   }
   return next();
 }
