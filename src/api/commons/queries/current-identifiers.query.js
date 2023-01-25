c74435b988d4928f0d49fd7724362ffef7d894f1
folland87 <frederic@olland.me>
2023-01-25 15:38:25 +0100

    fix structures light

---
+++ src/api/commons/queries/current-identifiers.query.js
@@ -0,0 +1,12 @@
+export default [
+  {
+    $lookup: {
+      from: 'identifiers',
+      let: { item: '$id' },
+      pipeline: [
+        { $match: { $expr: { $and: [{ $eq: ['$resourceId', '$$item'] }, { $eq: ['$active', true] }] } } },
+      ],
+      as: 'identifiers',
+    },
+  },
+];
