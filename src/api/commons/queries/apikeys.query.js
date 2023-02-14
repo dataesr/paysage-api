75f4dcc22bbf331f5402e02f9f481a371ef072e0
folland87 <frederic@olland.me>
2023-02-14 12:05:25 +0100

    return user with apiKey

--- src/api/commons/queries/apikeys.query.js
+++ src/api/commons/queries/apikeys.query.js
@@ -1,4 +1,5 @@
 import metas from './metas.query';
+import usersLightQuery from './users.light.query';
 
 export const onCreateApiKeyQuery = [
   ...metas,
@@ -21,12 +22,23 @@ export const onCreateApiKeyQuery = [
 export const onListApiKeyQuery = [
   ...metas,
   {
+    $lookup: {
+      from: 'users',
+      localField: 'userId',
+      foreignField: 'id',
+      pipeline: usersLightQuery,
+      as: 'user',
+    },
+  },
+  { $set: { user: { $arrayElemAt: ['$user', 0] } } },
+  {
     $project: {
       _id: 0,
       id: 1,
       name: 1,
       role: 1,
       userId: 1,
+      user: 1,
       createdBy: 1,
       updatedBy: 1,
       createdAt: 1,
