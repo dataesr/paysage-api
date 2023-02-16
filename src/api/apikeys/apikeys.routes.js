9f9050c073900905593f7d5f7bd2ecbec8fc518b
folland87 <frederic@olland.me>
2023-02-16 10:24:26 +0100

    api keys changes

--- src/api/apikeys/apikeys.routes.js
+++ src/api/apikeys/apikeys.routes.js
@@ -3,7 +3,7 @@ import { patchContext, createContext, setGeneratedInternalIdInContext } from '..
 import { saveInStore } from '../commons/middlewares/event.middlewares';
 import controllers from '../commons/middlewares/crud.middlewares';
 import { requireRoles } from '../commons/middlewares/rbac.middlewares';
-import { onCreateApiKeyQuery, onListApiKeyQuery } from '../commons/queries/apikeys.query';
+import { ApiKeyQuery } from '../commons/queries/apikeys.query';
 import { setApiKeyInContext, setUserIdInContext } from './apikeys.middlewares';
 import { apiKeysRepository as repository } from '../commons/repositories';
 import { apikeys as resource } from '../resources';
@@ -11,14 +11,14 @@ import { apikeys as resource } from '../resources';
 const router = new express.Router();
 
 router.route(`/admin/${resource}`)
-  .get(requireRoles(['admin']), controllers.list(repository, onListApiKeyQuery))
+  .get(requireRoles(['admin']), controllers.list(repository, ApiKeyQuery))
   .post([
     requireRoles(['admin']),
     createContext,
     setGeneratedInternalIdInContext(resource),
     setUserIdInContext,
     setApiKeyInContext,
-    controllers.create(repository, onCreateApiKeyQuery),
+    controllers.create(repository, ApiKeyQuery),
     saveInStore(resource),
   ]);
 
--- src/api/commons/queries/apikeys.query.js
+++ src/api/commons/queries/apikeys.query.js
@@ -1,25 +1,7 @@
 import metas from './metas.query';
 import usersLightQuery from './users.light.query';
 
-export const onCreateApiKeyQuery = [
-  ...metas,
-  {
-    $project: {
-      _id: 0,
-      id: 1,
-      name: 1,
-      role: 1,
-      apiKey: 1,
-      userId: 1,
-      createdBy: 1,
-      updatedBy: 1,
-      createdAt: 1,
-      updatedAt: 1,
-    },
-  },
-];
-
-export const onListApiKeyQuery = [
+export const ApiKeyQuery = [
   ...metas,
   {
     $lookup: {
@@ -37,6 +19,7 @@ export const onListApiKeyQuery = [
       id: 1,
       name: 1,
       role: 1,
+      apiKey: 1,
       userId: 1,
       user: 1,
       createdBy: 1,
