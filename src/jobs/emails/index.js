ec4387dcf793196089a581ab11dc015ee7572697
folland87 <frederic@olland.me>
2023-03-20 14:57:02 +0100

    test(mailer): restore the api and delete logger

--- src/jobs/emails/index.js
+++ src/jobs/emails/index.js
@@ -1,20 +1,16 @@
 import mailer from '../../services/mailer.service';
-import logger from '../../services/logger.service';
 
-export const sendAuthenticationEmail = async (job) => {
-  logger.info(job.attrs?.data);
-  return mailer.sendEmail({
-    to: [{ email: job.attrs?.data?.user?.email }],
-    templateId: 203,
-    params: {
-      PRENOM: job.attrs?.data?.user?.firstName,
-      NOM: job.attrs?.data?.user?.lastName,
-      EMAIL: job.attrs?.data?.user?.email,
-      CODE: job.attrs?.data?.otp,
-      IPVALIDATION: job.attrs?.data?.ip,
-    },
-  });
-};
+export const sendAuthenticationEmail = async (job) => mailer.sendEmail({
+  to: [{ email: job.attrs?.data?.user?.email }],
+  templateId: 203,
+  params: {
+    PRENOM: job.attrs?.data?.user?.firstName,
+    NOM: job.attrs?.data?.user?.lastName,
+    EMAIL: job.attrs?.data?.user?.email,
+    CODE: job.attrs?.data?.otp,
+    IPVALIDATION: job.attrs?.data?.ip,
+  },
+});
 
 export const sendWelcomeEmail = (job) => mailer.sendEmail({
   to: [{ email: job.attrs?.data?.user?.email }],
