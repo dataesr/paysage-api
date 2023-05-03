957c2bd97e6893c6ea48e4cb126e364cfa50dd39
folland87 <frederic@olland.me>
2023-05-03 15:32:48 +0200

    feat(jobs): add export gouvernance job

---
+++ src/api/opendata/opendata.routes.js
@@ -0,0 +1,23 @@
+import express from 'express';
+import { db } from '../../services/mongo.service';
+import { BadRequestError } from '../commons/http-errors';
+
+const router = new express.Router();
+
+const OPTIONS = { projection: { _id: 0, dataset: 0 } };
+
+const FILTERS = {
+  'fr-esr-annelis-paysage-gouvernance': { annelis: 'Y', dataset: 'fr-esr-annelis-paysage-gouvernance' },
+  'fr-esr-paysage-fonctions-gouvernance': { dataset: 'fr-esr-paysage-fonctions-gouvernance' },
+};
+
+router.route('/opendata/:datasetId')
+  .get(async (req, res) => {
+    const { datasetId } = req.params;
+    const query = FILTERS[datasetId];
+    if (!query) throw new BadRequestError('Unknown dataset');
+    const data = await db.collection('opendata').find(query, OPTIONS).toArray();
+    res.status(200).json(data);
+  });
+
+export default router;
