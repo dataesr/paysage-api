3ca7bd56e652759a12a949a847dfa9701763694f
folland87 <frederic@olland.me>
2023-05-10 11:19:34 +0200

    add filtering in curiexplore

--- src/api/curiexplore/curiexplore.routes.js
+++ src/api/curiexplore/curiexplore.routes.js
@@ -5,7 +5,9 @@ const router = new express.Router();
 
 router.route('/curiexplore/actors')
   .get(async (req, res) => {
-    const data = await db.collection('curiexploreactors').find({}).toArray();
+    const { filters } = req.query;
+    console.log(filters);
+    const data = await db.collection('curiexploreactors').find(filters).toArray();
     res.status(200).json(data);
   });
 
