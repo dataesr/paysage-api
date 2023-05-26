040f815e899ea770be14d9d0fdd89d3cde16f686
jerem <jeremy.peglion@gmail.com>
2023-05-26 10:21:30 +0200

    fix

--- src/api/geographicalcategories/exceptions/exceptions.routes.js
+++ src/api/geographicalcategories/exceptions/exceptions.routes.js
@@ -5,23 +5,23 @@ import controllers from '../../commons/middlewares/crud.middlewares';
 import { saveInStore } from '../../commons/middlewares/event.middlewares';
 import { readQuery } from '../../commons/queries/exceptions.query';
 import { geographicalCategoriesExceptionsRepository as repository } from '../../commons/repositories';
-import { exceptions as resource } from '../../resources';
+import { geographicalExceptions as resource } from '../../resources';
 
 const router = new express.Router();
 
 router.route(`/${resource}`)
     .post([
         createContext,
-        setGeneratedInternalIdInContext(subresource),
+        setGeneratedInternalIdInContext(resource),
         controllers.create(repository, readQuery),
-        saveInStore(subresource),
+        saveInStore(resource),
     ]);
 
 router.route(`/${resource}/:id`)
     .delete([
         patchContext,
         controllers.remove(repository),
-        saveInStore(subresource),
+        saveInStore(resource),
     ]);
 
 export default router;
