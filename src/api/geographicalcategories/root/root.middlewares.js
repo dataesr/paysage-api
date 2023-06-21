a2dba5b45b4ab9572c9c3ae04ee34d0d42da935c
Mihoub Debache <debache.mihoub@gmail.com>
2023-06-21 09:51:27 +0200

    fix(geographical-categories): get only current localisation on structure from geo categories

--- src/api/geographicalcategories/root/root.middlewares.js
+++ src/api/geographicalcategories/root/root.middlewares.js
@@ -1,20 +1,18 @@
 import { BadRequestError } from '../../commons/http-errors';
-
-import esClient from '../../../services/elastic.service';
-import config from '../../../config';
-import { geographicalCategoriesRepository as repository } from '../../commons/repositories';
-
-const { index } = config.elastic;
+import { geographicalCategoriesRepository as repository, structuresRepository } from '../../commons/repositories';
+import readQuery from '../../commons/queries/structures.query';
 
 export async function validatePayload(req, res, next) {
-  if (!Object.keys(req.body).length) throw new BadRequestError('Payload missing');
+  if (Object.keys(req.body).length === 0) {
+    throw new BadRequestError('Payload missing');
+  }
   return next();
 }
 
 export async function getGeographicalCategoryById(req, res, next) {
   const { id } = req.params;
   try {
-    const { data: geographicalCategory } = await repository.find({ filters: { id } });
+    const geographicalCategory = await repository.get(id);
     if (!geographicalCategory) {
       return res.status(404).json({ error: 'Geographical category not found' });
     }
@@ -28,33 +26,23 @@ export async function getGeographicalCategoryById(req, res, next) {
 export async function getStructureFromGeoCategory(req, res, next) {
   try {
     const { geographicalCategory } = req;
-    const body = JSON.stringify({
-      query: {
-        bool: {
-          must: {
-            match: {
-              type: 'structures',
-            },
-          },
-          filter: {
-            geo_shape: {
-              coordinates: {
-                shape: geographicalCategory[0].geometry,
-                relation: 'within',
-              },
-            },
-          },
+
+    const filters = {
+      'localisations.geometry': {
+        $geoWithin: {
+          $geometry: geographicalCategory.geometry,
         },
       },
-      size: 30,
-    });
+      'localisations.active': true,
+    };
 
-    const response = await esClient.search({ index, body });
-    const data = response.body?.hits?.hits?.map((hit) => hit?._source) || [];
+    filters['localisations.active'] = { $ne: false };
+
+    const { data } = await structuresRepository.find({ filters, useQuery: readQuery });
     res.status(200).json({ data, totalCount: data.length });
     next();
   } catch (error) {
-    res.status(500).json({ error: 'An error occurred while fetching data' });
+    res.status(500).json({ error: 'Une erreur s\'est produite lors de la récupération des données' });
     next(error);
   }
 }
