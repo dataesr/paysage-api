695ed548056f6630345f1fcd23be096eb51a2af4
Mihoub Debache <debache.mihoub@gmail.com>
2023-09-11 16:12:51 +0200

    fix(officialtext): update official text middlewares to throw error if jorftext already existe

--- src/api/officialtexts/officialtext.middlewares.js
+++ src/api/officialtexts/officialtext.middlewares.js
@@ -1,25 +1,37 @@
 import { client, db } from '../../services/mongo.service';
 import { BadRequestError, NotFoundError } from '../commons/http-errors';
-import { officialtextsRepository } from '../commons/repositories';
+import { officialtextsRepository,catalogRepository } from '../commons/repositories';
 
 export async function validatePayload(req, res, next) {
   if (!req.body || !Object.keys(req.body).length) throw new BadRequestError('Payload missing');
-  // const { relatesTo } = req.body;
-  // if (!relatesTo) return next();
-  // const { data } = await catalogRepository.find({ filters: { _id: { $in: relatesTo } } });
-  // const exists = data.reduce((arr, obj) => [...arr, obj._id], []);
-  // const notFound = relatesTo.filter((x) => exists.indexOf(x) === -1);
-  // if (notFound.length) {
-  //   throw new BadRequestError(
-  //     'Referencing unknown resource',
-  //     notFound.map((obj, i) => ({
-  //       path: `.body.relatesTo[${i}]`,
-  //       message: `Reference error: '${obj}' does not exist`,
-  //     })),
-  //   );
-  // }
+  const { relatesTo, jorftext } = req.body; 
+  if (!relatesTo) return next();
+
+  const { data } = await catalogRepository.find({ filters: { _id: { $in: relatesTo } } });
+  const exists = data.reduce((arr, obj) => [...arr, obj._id], []);
+  const notFound = relatesTo.filter((x) => exists.indexOf(x) === -1);
+
+  const jorftextFromDB = await officialtextsRepository.findOne({ jorftext });
+
+  if (jorftextFromDB?.jorftext && typeof jorftextFromDB === 'object') {
+    throw new BadRequestError(
+      `Reference error: '${jorftext}' already exists`
+    );
+}
+
+  if (notFound.length) {
+    throw new BadRequestError(
+      'Referencing unknown resource',
+      notFound.map((obj, i) => ({
+        path: `.body.relatesTo[${i}]`,
+        message: `Reference error: '${obj}' does not exist`,
+      })),
+    );
+  }
   return next();
 }
+
+
 
 export async function deleteOfficialText(req, res, next) {
   const { id: resourceId } = req.params;
