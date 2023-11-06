1da6891b254a6a8b83d7ebc5081b6f14fa3a54df
Mihoub Debache <debache.mihoub@gmail.com>
2023-11-06 13:48:52 +0100

    fix(geographical-categories): update elastic query to get more info

--- src/api/commons/queries/geographical-categories.elastic.js
+++ src/api/commons/queries/geographical-categories.elastic.js
@@ -3,12 +3,58 @@ import metas from './metas.query';
 export default [
   ...metas,
   {
+    $lookup: {
+      from: 'geographicalcategories',
+      localField: 'parentOriginalId',
+      foreignField: 'originalId',
+      pipeline: [{
+        $project: {
+          _id: 0,
+          id: 1,
+          level: 1,
+          nameEn: { $ifNull: ['$nameEn', null] },
+          nameFr: { $ifNull: ['$nameFr', null] },
+          originalId: 1,
+          parentOriginalId: { $ifNull: ['$parentOriginalId', null] },
+        },
+      }],
+      as: 'parent',
+    },
+  },
+  { $set: { parent: { $arrayElemAt: ['$parent', 0] } } },
+  {
+    $lookup: {
+      from: 'geographicalcategories',
+      localField: 'originalId',
+      foreignField: 'parentOriginalId',
+      pipeline: [{
+        $project: {
+          _id: 0,
+          id: 1,
+          level: 1,
+          nameEn: { $ifNull: ['$nameEn', null] },
+          nameFr: { $ifNull: ['$nameFr', null] },
+          originalId: 1,
+          parentOriginalId: { $ifNull: ['$parentOriginalId', null] },
+        },
+      }],
+      as: 'children',
+    },
+  },
+  {
     $project: {
       _id: 0,
+      academyParent: { $ifNull: ['$academyParent', null] },
+      children: { $ifNull: ['$children', null] },
+      geometry: { $ifNull: ['$geometry', null] },
       id: 1,
+      level: { $ifNull: ['$level', null] },
+      nameEn: { $ifNull: ['$nameEn', null] },
       nameFr: 1,
-      level: 1,
-      isDeleted: { $ifNull: ['$isDeleted', false] },
+      originalId: { $ifNull: ['$originalId', null] },
+      parent: { $ifNull: ['$parent', null] },
+      priority: { $ifNull: ['$priority', null] },
+      wikidata: { $ifNull: ['$wikidata', null] },
     },
   },
 ];
