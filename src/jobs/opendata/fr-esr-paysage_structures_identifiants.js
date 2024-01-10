6c593c8153d51ea24b0edae5e0deb62648093498
Mihoub Debache <debache.mihoub@gmail.com>
2024-01-10 10:37:09 +0100

    feat(task): add new task for structure identifiers and a new route for identifiers

---
+++ src/jobs/opendata/fr-esr-paysage_structures_identifiants.js
@@ -0,0 +1,44 @@
+import structuresLightQuery from '../../api/commons/queries/structures.light.query';
+import { client, db } from '../../services/mongo.service';
+
+const dataset = 'fr-esr-paysage_structures_identifiants';
+
+export default async function exportFrEsrStructureIdentifiers() {
+  const data = await db.collection('identifiers').aggregate([
+    {
+      $lookup: {
+        from: 'structures',
+        localField: 'resourceId',
+        foreignField: 'id',
+        pipeline: structuresLightQuery,
+        as: 'structure',
+      },
+    },
+    { $set: { structure: { $arrayElemAt: ['$structure', 0] } } },
+  ]).toArray();
+  const json = data.map(({ structure, ...identifier }) => {
+    if (!structure || !structure.id || !identifier || !identifier.id) {
+      return null;
+    }
+
+    const row = {
+      dataset,
+      id: identifier.value,
+      id_structure_paysage: structure.id,
+      id_type: identifier.type,
+      status: identifier.active,
+      id_startDate: identifier.startDate,
+      id_endDate: identifier.endDate,
+    };
+    return row;
+  }).filter((row) => row !== null);
+
+  const session = client.startSession();
+  await session.withTransaction(async () => {
+    await db.collection('opendata').deleteMany({ dataset });
+    await db.collection('opendata').insertMany(json);
+    await session.endSession();
+  });
+
+  return { status: 'success', location: `/opendata/${dataset}` };
+}
