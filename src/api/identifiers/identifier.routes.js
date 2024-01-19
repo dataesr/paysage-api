a94851525ca6e02bec28ede1de6d6e1f816cec4b
Mihoub Debache <debache.mihoub@gmail.com>
2024-01-19 14:21:47 +0100

    fix(identifiers & weblinks): update route, avoid error 500 from server

--- src/api/identifiers/identifier.routes.js
+++ src/api/identifiers/identifier.routes.js
@@ -2,10 +2,11 @@ import express from 'express';
 import controllers from '../commons/middlewares/crud.middlewares';
 import { identifiersRepository as repository } from '../commons/repositories';
 import { identifiers as resource } from '../resources';
+import { readQuery } from '../commons/queries/identifiers.query';
 
 const router = new express.Router();
 
 router.route(`/${resource}`)
-  .get(controllers.list(repository));
+  .get(controllers.list(repository, readQuery));
 
 export default router;
