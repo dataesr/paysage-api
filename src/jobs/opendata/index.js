dd735e55af6ab0740e7cbdf200cf2afbb25b9ee1
Mihoub Debache <debache.mihoub@gmail.com>
2024-02-01 09:30:20 +0100

    feat(jobs): add prizes jobs to creat dataset

--- src/jobs/opendata/index.js
+++ src/jobs/opendata/index.js
@@ -3,3 +3,4 @@ export { default as exportFrEsrAnnelisPaysageEtablissements } from './fr-esr-ann
 export { default as exportFrEsrStructureIdentifiers } from './fr-esr-paysage_structures_identifiants';
 export { default as exportFrEsrPersonIdentifiers } from './fr-esr-paysage_personnes_identifiants';
 export { default as exportFrEsrStructureWebsites } from './fr-esr-paysage_structures_websites';
+export { default as exportFrEsrPrizes } from './fr-esr-paysage_prix';
