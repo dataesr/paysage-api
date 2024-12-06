d47ba1a63478fc587bbcb475d28128a3a43bf415
folland87 <frederic@olland.me>
2024-12-06 14:50:44 +0100

    fix

--- src/jobs/sirene/config.js
+++ src/jobs/sirene/config.js
@@ -1,3 +1,3 @@
 export const SIREN_API_URL = "https://api.insee.fr/api-sirene/3.11";
-export const SIREN_API_KEY = "*****";
+export const SIREN_API_KEY = process.env.SIREN_API_KEY;
 export const SIREN_TASK_NAME = "sync-sirene";
